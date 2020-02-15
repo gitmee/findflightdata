7afd2c2bda40aa6fd8f697530728a429f9df7eed
Fion <fion.sun@gmail.com>
2020-02-14 21:14:58 -0800

    Add files via upload
    workable version

--- findflightdata.py
+++ findflightdata.py
@@ -1,20 +1,54 @@
-frame = input("Enter frame #: ")
+# -----------------------------------------------------------------------
+# finds recorded flight data closest to the timeStamp indicated by frame
+# for ortho-rectification in Headwall's SpectralView
+# 
+# 2/14/2020, Cal Poly Pomona
+# email: fionchan@cpp.edu
+# -----------------------------------------------------------------------
+from pathlib import Path
 
-filename = "frameIndex_" + frame + ".txt"
-#print(filename)
-frameIndex = open(filename, "r")
-for i in [0,1]:
-	line = frameIndex.readline()
-#print(line)
-timeStamp = line.split('\t')[1]
-#print(timeStamp)
-frameIndex.close()
-line = ""
+folder = Path(input("Enter folder path: "))
 
-imu_gps = open("imu_gps.txt","r")
-#read imu_gps line by line
-#until find closest matching timeStamp (col7 == timeStamp)
-#col1 = roll; col2 = pitch; col3 = yaw; etc...
-#each col is delineated by '\t'
+start = "y"
+while start == "y" or start == "Y":
+	# find timeStamp from frameIndex.txt
+	frame = input("Enter frame #: ")
+	filepath = folder / ("frameIndex_" + str(frame) + ".txt")
+	print(filepath)
+	frameIndex = open(filepath, "rt")
+	for i in [0,1]:
+		line = frameIndex.readline()
+	timeStamp = line.split('\t')[1]
+	print("Indicated Timestamp: " + timeStamp)
+	frameIndex.close()
+
+	# read imu_gps.txt line by line
+	# until find closest matching timeStamp (col6 == timeStamp)
+	# col0 = roll; col1 = pitch; col2 = yaw; etc...
+	# each col is delineated by '\t'
+	imu_gps = open(folder / "imu_gps.txt", "rt")
+	header = imu_gps.readline().split('\t',9)
+	found = False;
+	while found == False:
+		row = imu_gps.readline().split('\t',9)
+		nextRow = imu_gps.readline().split('\t',9)
+		if nextRow:
+			if nextRow[6] > timeStamp:
+				found = True
+				print("Found:")
+				ts1 = int(row[6])
+				nextRow = imu_gps.readline().split('\t',9)
+				ts2 = int(nextRow[6])
+				timeStamp = int(timeStamp)
+				if abs(timeStamp - ts1) < abs(ts2 - timeStamp):
+					for i in range(0,10):
+						print(header[i] + ": " + row[i])
+				else:
+					for i in range(0,10): 
+						print(header[i] + ": " + nextRow[i])
+		else:
+			break
+
+	start = input("Search another frame? (y/n): ")
 
 imu_gps.close()
